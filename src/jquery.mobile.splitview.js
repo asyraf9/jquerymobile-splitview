21aa7daa956230c20b1716879cb9122e2848ebc4
Randy Esplin <randy.esplin@ecinternet.com>
2013-11-27 14:38:33 -0800

    Fixed pixel ratio issue

--- src/jquery.mobile.splitview.js
+++ src/jquery.mobile.splitview.js
@@ -14,7 +14,7 @@
     if($.support.touch){
       $('html').addClass('touch');
     }
-    var $query = $.mobile.media('screen and (min-width: 480px)') && ($.mobile.media('(-webkit-max-device-pixel-ratio: 1.2)') || $.mobile.media('(max--moz-device-pixel-ratio: 1.2)'));
+    var $query = $.mobile.media('screen and (min-width: 480px)') && ($.mobile.media('(-webkit-max-device-pixel-ratio: 2.2)') || $.mobile.media('(max--moz-device-pixel-ratio: 2.2)'));
     $.support.splitview = ($query || ($.mobile.browser.ie && $(this).width() >= 480)) && $.mobile.ajaxEnabled;
     if ($.support.splitview) {
       $('html').addClass('splitview');
